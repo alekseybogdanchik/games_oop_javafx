a56e352674ca5795349ebf787462d014f155cd5d
alekseybogdanchik <abp163@mail.ru>
2020-05-10 16:22:38 +0400

    Крестики-нолики на JavaFX [#165133]

--- tictactoe/src/test/java/job4j/tictactoe/Logic3TTest.java
+++ tictactoe/src/test/java/job4j/tictactoe/Logic3TTest.java
@@ -68,22 +68,33 @@ public class Logic3TTest {
     @Test
     public void whenHasOWinner() {
         Figure3T[][] table = {
-                {new Figure3T(true, false), new Figure3T(), new Figure3T()},
-                {new Figure3T(true, false), new Figure3T(true, false), new Figure3T()},
-                {new Figure3T(true, false), new Figure3T(), new Figure3T(true, false)},
+                {new Figure3T(true, false), new Figure3T(false, true), new Figure3T()},
+                {new Figure3T(), new Figure3T(false, true), new Figure3T(true, false)},
+                {new Figure3T(), new Figure3T(false, true), new Figure3T()},
         };
         Logic3T login = new Logic3T(table);
         assertThat(login.isWinnerO(), is(true));
     }
 
     @Test
-    public void whenHasGas() {
+    public void whenHasGap() {
         Figure3T[][] table = {
                 {new Figure3T(true, false), new Figure3T(), new Figure3T()},
                 {new Figure3T(), new Figure3T(true, false), new Figure3T()},
-                {new Figure3T(), new Figure3T(), new Figure3T(true, false)},
+                {new Figure3T(), new Figure3T(), new Figure3T(false, true)},
         };
         Logic3T login = new Logic3T(table);
         assertThat(login.hasGap(), is(true));
+    }
+
+    @Test
+    public void whenNoGap() {
+        Figure3T[][] table = {
+                {new Figure3T(true, false), new Figure3T(false, true), new Figure3T(true, false)},
+                {new Figure3T(true, false), new Figure3T(false, true), new Figure3T(true, false)},
+                {new Figure3T(false, true), new Figure3T(true, false), new Figure3T(false, true)},
+        };
+        Logic3T login = new Logic3T(table);
+        assertThat(login.hasGap(), is(false));
     }
 }
